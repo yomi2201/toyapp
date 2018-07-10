f4d806f783985c80c0b6c5123cb33c711b67a71f
yomi2201 <brbr091429@yahoo.co.jp>
2018-07-10 09:02:09 +0900

    toyFinish

---
+++ vendor/bundle/ruby/2.5.0/gems/nokogiri-1.8.4/ports/x86_64-pc-linux-gnu/libxml2/2.9.8/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/yomi/rails/myapp/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.8.4/ports/x86_64-pc-linux-gnu/libxml2/2.9.8/lib"
+XML2_LIBS="-lxml2 -lz     -lm "
+XML2_INCLUDEDIR="-I/home/yomi/rails/myapp/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.8.4/ports/x86_64-pc-linux-gnu/libxml2/2.9.8/include/libxml2"
+MODULE_VERSION="xml2-2.9.8"
+
